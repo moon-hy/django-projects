ad93b337c300b76f4b1567fb53e5d6362b4481b8
hy.moon <moraramee@gmail.com>
2022-01-10 01:46:29 +0900

    Update navmenu.html

--- board/views/base_views.py
+++ board/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.checks import messages
 from django.utils import timezone
-from board.models import Post, Notification, User, Category
+from board.models import Post, Category
 from django.db.models import Q, Count
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
@@ -49,37 +49,8 @@ def category(request, category_url):
 
     context = {'posts': page_obj, 'page': page, 'kw': kw, 'so': so, 'category': category}
     return render(request, 'board/post_list.html', context)
+
     
-'''
-def index(request):
-    page = request.GET.get('page', '1')
-    kw = request.GET.get('kw', '')
-    so = request.GET.get('so', 'recent')  # 정렬기준
-
-    # 정렬
-    if so == 'like':
-        post_list = Post.objects.annotate(num_liked=Count('liked')).order_by('-num_liked', '-create_date')
-    elif so == 'comment':
-        post_list = Post.objects.annotate(num_comment=Count('comment')).order_by('-num_comment', '-create_date')
-    elif so == 'hit':
-        post_list = Post.objects.order_by('-hits', '-create_date')
-    else:  # recent
-        post_list = Post.objects.order_by('-create_date')
-
-    if kw:
-        post_list = post_list.filter(
-            Q(subject__icontains=kw)|
-            Q(content__icontains=kw)|
-            Q(author__username__icontains=kw)|
-            Q(comment__author__username__icontains=kw)
-        ).distinct()
-
-    paginator = Paginator(post_list, 10)
-    page_obj = paginator.get_page(page)
-
-    context = {'posts': page_obj, 'page': page, 'kw': kw, 'so': so}
-    return render(request, 'board/post_list.html', context)
-'''
 @login_required(login_url='common:login')
 def detail(request, post_id):
     try:
